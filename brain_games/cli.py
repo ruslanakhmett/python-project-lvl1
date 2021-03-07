6047aa844217119cfad5083e4cdd57fb367c0bfb
Ruslan Akhmetshin <73955661+ruslanakhmett@users.noreply.github.com>
2021-03-07 19:36:12 +0300

    Create cli.py

---
+++ brain_games/cli.py
@@ -0,0 +1,17 @@
+
+"""CLI functions."""
+
+import prompt
+
+
+def welcome_user():
+    """
+    Ask for user's name.
+    Return user's name
+    Returns:
+        str
+    """
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print('Hello, {0}'.format(user_name))
+    return user_name
